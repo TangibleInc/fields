7c5eaa40de585691956abc558790cff1119cc612
Nicolas Jaussaud <nicolas@teamtangible.com>
2023-01-26 19:06:55 -0500

    Editor: Fix issue when initiliazed from elementor

--- assets/src/components/field/editor/Editor.jsx
+++ assets/src/components/field/editor/Editor.jsx
@@ -22,15 +22,22 @@ const Editor = props => {
   } = useTextField(props, ref)
 
   const [value, setValue] = useState(props.value)
-
+  
   useEffect(() => {
-    tinyMCE.init({
-      target: ref.current,
-      // Customize editor options: https://www.tiny.cloud/docs/general-configuration-guide/basic-setup/
-      setup: function(editor){
-        editor.on('input', () => setValue(editor.getContent()))
-        editor.on('ExecCommand', () => setValue(editor.getContent()))
-      }
+    
+    /**
+     * Calling tinyMCE.init directly can fail in some cases, wrapping it 
+     * inside a setTimeout seems to fix the issue
+     */
+    setTimeout(() => {
+      tinyMCE.init({
+        target: ref.current,
+        // Customize editor options: https://www.tiny.cloud/docs/general-configuration-guide/basic-setup/
+        setup: function(editor){
+          editor.on('input', () => setValue(editor.getContent()))
+          editor.on('ExecCommand', () => setValue(editor.getContent()))
+        }
+      })
     })
   }, [])
 
