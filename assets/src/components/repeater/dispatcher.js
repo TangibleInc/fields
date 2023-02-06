f38b79aa431b4c57eb029e9c2ac7cf7bc828f853
Reimar Rosas <reimar@reimarrosas.me>
2023-02-06 12:44:32 -0500

    Cleanup unused/unlabeled code

--- assets/src/components/repeater/dispatcher.js
+++ assets/src/components/repeater/dispatcher.js
@@ -1,42 +1,34 @@
-import { uniqid } from '../../utils'
+import { uniqid } from "../../utils";
 
-const repeaterDispatcher = emptyItem => (
-  (items, action) => {
-    switch (action.type) {
-      case 'add':
-        return [ 
-          ...items, 
-          { 
-            key: uniqid(), 
-            ...(action.data ?? emptyItem) 
-          } 
-        ]
-      case 'remove':
-        return [
-          ...items.slice(0, action.item),
-          ...items.slice(action.item + 1)
-        ]  
-      case 'update':
-        items[ action.item ][ action.control ] = action.value
-        return [ 
-          ...items
-        ]
-      case 'clear': return []
-      default: return items
-    }
+const repeaterDispatcher = (emptyItem) => (items, action) => {
+  switch (action.type) {
+    case "add":
+      return [
+        ...items,
+        {
+          key: uniqid(),
+          ...(action.data ?? emptyItem),
+        },
+      ];
+    case "remove":
+      return [...items.slice(0, action.item), ...items.slice(action.item + 1)];
+    case "update":
+      items[action.item][action.control] = action.value;
+      return [...items];
+    case "clear":
+      return [];
+    default:
+      return items;
   }
-)
+};
 
-const initDispatcher = value => {
+const initDispatcher = (value) => {
+  const initialItems = value !== "" ? JSON.parse(value) : false;
 
-  const initialItems = value !== '' ? JSON.parse(value) : false
+  const isNonEmptyArray =
+    Array.isArray(initialItems) && initialItems.length !== 0;
 
-  return Array.isArray(initialItems) && initialItems.length !== 0
-    ? initialItems
-    : [ {} ] 
-}
+  return isNonEmptyArray ? initialItems : [{}];
+};
 
-export {
-  repeaterDispatcher,
-  initDispatcher
-}
+export { repeaterDispatcher, initDispatcher };
