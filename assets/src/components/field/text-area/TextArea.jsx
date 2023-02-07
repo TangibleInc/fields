edd063503884fe5d538da3ffbb4208b458ebc14d
Reimar Rosas <reimar@reimarrosas.me>
2023-02-07 15:29:04 -0500

    Modified the TextArea implementation to take into account ACF fields based on the conversation in the PR comments.
    Note: Wrapper attributes still not implemented

--- assets/src/components/field/text-area/TextArea.jsx
+++ assets/src/components/field/text-area/TextArea.jsx
@@ -1,28 +1,35 @@
-import { useRef } from 'react';
-import { useTextField } from 'react-aria';
+import { useRef } from "react";
+import { useTextField } from "react-aria";
 
-import { Description, Label } from '../../base';
+import { Description, Label } from "../../base";
 
 /**
  * @see https://react-spectrum.adobe.com/react-aria/useTextField.html
  */
 
-const TextArea = props => {
-    const ref = useRef()
-    
-    const {
-      labelProps,
-      inputProps,
-      descriptionProps
-    } = useTextField({ ...props, inputElementType: 'textarea' }, ref)
+const TextArea = (props) => {
+  const ref = useRef();
 
-    return (
-      <div className="tf-text-area">
-        {props.label && <Label { ...labelProps }>{ props.label }</Label>}
-        <textarea { ...inputProps } ref={ ref }></textarea>
-        {props.description && <Description { ...descriptionProps }>{ props.description }</Description>}
-      </div>
-    )
-}
+  const { labelProps, inputProps, descriptionProps } = useTextField(
+    { ...props, inputElementType: "textarea" },
+    ref
+  );
+
+  return (
+    <div className="tf-text-area">
+      {props.label && <Label {...labelProps}>{props.label}</Label>}
+      <textarea
+        {...inputProps}
+        maxLength={props.maxlength}
+        required={props.required}
+        rows={props.rows}
+        ref={ref}
+      ></textarea>
+      {props.description && (
+        <Description {...descriptionProps}>{props.description}</Description>
+      )}
+    </div>
+  );
+};
 
 export default TextArea;
