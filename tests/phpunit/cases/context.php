7c1da9c7aaed5dc643d46757eda5670914be87a7
Gennady Kovshenin <gennady@kovshenin.com>
2023-03-01 15:58:06 +0300

    Tests: context.php

---
+++ tests/phpunit/cases/context.php
@@ -0,0 +1,24 @@
+<?php
+class Context_TestCase extends WP_UnitTestCase {
+	public function test_fields_context() {
+		$this->assertEquals(['default'], tangible_fields()->contexts);
+		$this->assertEquals('default', tangible_fields()->current_context);
+		$this->assertEmpty(tangible_fields()->enqueued_contexts);
+
+		tangible_fields()->set_context('unknown');
+		$this->assertEquals(['default'], tangible_fields()->contexts);
+		$this->assertEquals('default', tangible_fields()->current_context);
+		$this->assertEmpty(tangible_fields()->enqueued_contexts);
+
+		tangible_fields()->set_context('default');
+		$this->assertEquals(['default'], tangible_fields()->contexts);
+		$this->assertEquals('default', tangible_fields()->current_context);
+		$this->assertEquals(['default'], tangible_fields()->enqueued_contexts);
+
+		tangible_fields()->contexts []= 'custom';
+		tangible_fields()->set_context('custom');
+		$this->assertEquals(['default', 'custom'], tangible_fields()->contexts);
+		$this->assertEquals('custom', tangible_fields()->current_context);
+		$this->assertEquals(['default', 'custom'], tangible_fields()->enqueued_contexts);
+	}
+}
