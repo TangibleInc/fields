9e5e812028c14150d695d3a23f7aada8d4e78d52
Julien Kappler <julien@teamtangible.com>
2023-03-15 14:35:44 +0100

    Accordion : Finish new field accordion

--- assets/src/components/field/accordion/Accordion.jsx
+++ assets/src/components/field/accordion/Accordion.jsx
@@ -1,58 +1,82 @@
 import { useState, useEffect, useReducer } from 'react'
 import { Button } from '../../base'
 import Control from '../../../Control'
+import { applyDynamicValues } from '../../../dynamic'
+import { initDispatcher, repeaterDispatcher } from '../../repeater/dispatcher'
 
 const Accordion = props => {
 
-    console.log(props)
     const [showItem, setShowItem] = useState(true)
     const toggleShow = () => setShowItem( !showItem )
 
     const fields = props.fields ?? []
+    const maxLength = Infinity
 
-    console.log(Array.prototype.values(fields))
-
-    const rowFields = fields.maps( (field, label) => {
-
-        const rowField = Object.assign({}, field)
-        
-        const layout = field.layout ?? ''
-
-        if( layout === 'table' ) {
-            delete rowField.label
-            delete rowField.description
-        }
-        
-        return rowField
-    })
-
-    console.log(rowFields)
+    const emptyItem = {}
+    fields.forEach(field => emptyItem[ field.name ] = '')
 
     const [items, dispatch] = useReducer(
-        repeaterDispatcher(emptyItem, maxLength), 
-        props.value ?? '',
-        initDispatcher
-      )
+      repeaterDispatcher(emptyItem, maxLength), 
+      props.value ?? '',
+      initDispatcher
+    ) 
 
-    const getControl = (control, item, i) => (
-        <Control
-          value={ item[control.name] ?? '' }
-          onChange={ value => dispatch({ 
-            type    : 'update',
-            item    : i,
-            control : control.name,
-            value   : value
-          }) }
-          { ...control }
-        />
+    const rowFields = fields.map(field => {
+
+      const rowField = Object.assign({}, field)
+      
+      if( field.layout && field.layout === 'table' ) {
+        delete rowField.label
+        delete rowField.description
+      }
+
+      delete rowField.value
+      delete rowField.onChange
+      
+      return rowField
+    })
+
+    const getRow = item => (
+      applyDynamicValues(
+        props.element ?? false,
+        rowFields,
+        item
+      )
     )
 
+    useEffect(() => {
+      props.onChange(items)
+    }, [items])
+
     return (
-        <div class='tf-repeater-block-items'>
-            <Button type="action" onPress={ () => toggleShow() }>
-              { showItem ? 'Close' : 'Edit' }
-            </Button>
-            test
+        <div class='tf-accordion-items' >
+            <div class='tf-accordion-header' onClick={ () => toggleShow() } >
+              <div class='tf-accordion-group-buttons' >
+                <span class={showItem ? 'tf-accordion-arrow tf-accordion-arrow-up' : 'tf-accordion-arrow tf-accordion-arrow-down'} />
+              </div>
+            </div>
+            {
+              showItem ?
+                items.map((item, i) => {
+                  return getRow(item).map(
+                    control => {
+                      return <div class="tf-accordion-item">
+                        <Control 
+                          value={ item[control.name] ?? '' }
+                          onChange={ value => dispatch({ 
+                            type    : 'update',
+                            item    : i,
+                            control : control.name,
+                            value   : value
+                          }) }
+                          { ...control }
+                        />
+                      </div>
+                    }
+                  )
+                })
+              : ''
+            }
         </div>
     )
 }
