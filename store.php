7621a1644c063c4d9a6d6d5098361ad1af932cc4
Gennady Kovshenin <gennady@kovshenin.com>
2023-03-17 21:22:26 +0300

    Feat: permission callbacks, wp_options store
    Needs tests.

--- store.php
+++ store.php
@@ -43,15 +43,17 @@ $fields->store_value = function (
  * A collection of reusable store/fetch callbacks.
  *
  * @usage tangible_fields()->register_field('name', [
- *  'type',
+ *  'type' => 'text',
  *  ...tangible_fields()->_store_callbacks['memory']()
- *  'permission_callback' => '__return_true',
+ *  ...tangible_fields()->_permission_callback['user_can']('manage_options'),
  * ];
  *
  * Available callbacks and their parameters:
  *
  *  - memory: an ephemeral (request lifetime) store
  *    mostly used for testing. Parameters: none.
+ *  - options: use the wp_options table. Parameters:
+ *    - $prefix - the option name prefix
  */
 $fields->_store_callbacks = [
   'memory' => function () {
@@ -66,4 +68,37 @@ $fields->_store_callbacks = [
       }
     ];
   },
+  'options' => function ($prefix = 'tanbile_fields_') {
+    return [
+      'store_callback' => function ($name, $value) use ($prefix) {
+        return update_option( "{$prefix}{$name}", $value );
+      },
+      'fetch_callback' => function ($name) use ($prefix) {
+        return get_option( "{$prefix}{$name}" );
+      },
+    ];
+  },
+];
+
+/**
+ * A collection of reusable permissions callbacks.
+ *
+ * @usage tangible_fields()->register_field('name', [
+ *  'type' => 'text',
+ *  ...tangible_fields()->_store_callbacks['memory'](),
+ *  ...tangible_fields()->_permission_callback['user_can']('manage_options'),
+ * ];
+ *
+ * Available callbacks and their parameters:
+ *
+ *  - user_can: uses the `current_user_can` WordPress function
+ *    to verify current user permissions. Parameters:
+ *    - $permission - the permission name
+ *    - $args - arguments to pass to the function
+ *      (if callable will be called and spread instead)
+ */
+$fields->_permission_callbacks = [
+  'user_can' => function ($permission, $args = null) {
+    return current_user_can( $permission, ...(is_callable( $args ) ? $args() : ($args ?? [])) );
+  }
 ];
