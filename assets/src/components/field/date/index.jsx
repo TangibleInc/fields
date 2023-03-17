423c246c422efe552bf7d0319b40ff29d4e248f3
Adrian Buenavista <adrian@teamtangible.com>
2023-03-17 10:17:46 +0800

    Select a date in the future (optional)

--- assets/src/components/field/date/index.jsx
+++ assets/src/components/field/date/index.jsx
@@ -18,8 +18,23 @@ import DatePicker from './DatePicker'
  */
 export default props => {
 
-  const minValue = today(getLocalTimeZone())  
+
+  const dateToday = today( getLocalTimeZone() )
+  const noDateLimit = () => {
+
+    dateToday.day = 1
+    dateToday.month = 1
+    dateToday.year = 1
+
+    return dateToday
+
+  }
+
+  /* 'future_only' => true, Add this to render field (date picker) to enable the future date only */
+  const hasFutureOnly = props.future_only && props.future_only === true
+  const minValue = hasFutureOnly ? dateToday : props.value ? noDateLimit() : dateToday
   const [value, setValue] = useState()
+
 
   /**
    * Init value on first render
@@ -34,10 +49,27 @@ export default props => {
       ? new CalendarDate('AD', initialValue[0], initialValue[1], initialValue[2])
       : minValue
     )
+
   }, [])
 
   useEffect(() => {
+
     props.onChange && props.onChange( getStringValue() )
+
+    if( hasFutureOnly && value && dateToday && props.value ){
+
+      let yearRule = value.year < dateToday.year,
+          monthRule = value.year <= dateToday.year && value.month < dateToday.month,
+          dayRule = value.year <= dateToday.year && value.month <= dateToday.month && value.day < dateToday.day
+      
+      if( yearRule || monthRule || dayRule ) 
+        setValue(new CalendarDate('AD',
+        yearRule ? dateToday.year : value.year,
+        monthRule ? dateToday.month : value.month,
+        dayRule ? dateToday.day : value.day))
+
+    }
+
   }, [value])
 
   const getStringValue = () => (
