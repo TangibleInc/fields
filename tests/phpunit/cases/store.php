ece0cf546ea96a33317179f02bcc89c9c64a8960
Nicolas Jaussaud <nicolas@teamtangible.com>
2023-04-24 13:43:42 -0400

    Tests: Compatibility with PHP 7.4 (Remove spread operator on associative array and use public visibility on data provider)

--- tests/phpunit/cases/store.php
+++ tests/phpunit/cases/store.php
@@ -5,10 +5,10 @@ class Store_TestCase extends TF_UnitTestCase {
 	}
 
 	public function test_fields_store_store_callbacks_memory() {
-		tangible_fields()->register_field('test', [
-			...tangible_fields()->_store_callbacks['memory'](),
-			'permission_callback' => '__return_true',
-		]);
+		tangible_fields()->register_field('test', 
+      [ 'permission_callback' => '__return_true' ] 
+      + tangible_fields()->_store_callbacks['memory']()
+    );
 
 		$value = tangible_fields()->fetch_value('test');
 		$this->assertNull($value);
@@ -21,10 +21,10 @@ class Store_TestCase extends TF_UnitTestCase {
 	}
 
 	public function test_fields_store_store_callbacks_options() {
-		tangible_fields()->register_field('test', [
-			...tangible_fields()->_store_callbacks['options']('test_'),
-			'permission_callback' => '__return_true',
-		]);
+		tangible_fields()->register_field('test', array_merge(
+			tangible_fields()->_store_callbacks['options']('test_'),
+			[ 'permission_callback' => '__return_true' ],
+		));
 
 		$this->assertFalse(get_option('test_test'));
 
@@ -35,10 +35,10 @@ class Store_TestCase extends TF_UnitTestCase {
 		$this->assertTrue(tangible_fields()->store_value('test', null)); // Cleanup.
 		$this->assertFalse(get_option('test_test'));
 
-		tangible_fields()->register_field('default', [
-			...tangible_fields()->_store_callbacks['options'](),
-			'permission_callback' => '__return_true',
-		]);
+		tangible_fields()->register_field('default', array_merge(
+			tangible_fields()->_store_callbacks['options'](),
+			[ 'permission_callback' => '__return_true' ],
+		));
 
 		$this->assertTrue(tangible_fields()->store_value('default', 'store'));
 		$this->assertEquals('store', get_option('tf_default'));
@@ -50,10 +50,10 @@ class Store_TestCase extends TF_UnitTestCase {
 	public function test_fields_store_store_callbacks_meta_post() {
 		$id = wp_insert_post(['post_title' => 'A Tangible Post']);
 
-		tangible_fields()->register_field('test', [
-			...tangible_fields()->_store_callbacks['meta']('post', $id, 'test_'),
-			'permission_callback' => '__return_true',
-		]);
+		tangible_fields()->register_field('test', array_merge(
+			tangible_fields()->_store_callbacks['meta']('post', $id, 'test_'),
+			[ 'permission_callback' => '__return_true' ],
+		));
 
 		$this->assertEmpty(get_post_meta($id, 'test_test', true));
 
@@ -73,10 +73,10 @@ class Store_TestCase extends TF_UnitTestCase {
 			return $id;
 		};
 
-		tangible_fields()->register_field('test', [
-			...tangible_fields()->_store_callbacks['meta']('user', $the_user_id),
-			'permission_callback' => '__return_true',
-		]);
+		tangible_fields()->register_field('test', array_merge(
+			tangible_fields()->_store_callbacks['meta']('user', $the_user_id),
+			[ 'permission_callback' => '__return_true' ],
+		));
 
 		$this->assertEmpty(get_user_meta($id, 'tf_test', true));
 
@@ -94,15 +94,17 @@ class Store_TestCase extends TF_UnitTestCase {
 	public function test_fields_store_permission_custom_permissions() {
 		$allowed = false;
 
-		tangible_fields()->register_field('test', [
-			...tangible_fields()->_store_callbacks['memory'](),
-			'permission_callback_store' => function() use (&$allowed) {
-				return $allowed;
-			},
-			'permission_callback_fetch' => function() use (&$allowed) {
-				return $allowed;
-			},
-		]);
+		tangible_fields()->register_field('test', array_merge(
+			tangible_fields()->_store_callbacks['memory'](),
+      [
+			  'permission_callback_store' => function() use (&$allowed) {
+				  return $allowed;
+			  },
+			  'permission_callback_fetch' => function() use (&$allowed) {
+				  return $allowed;
+			  },
+      ]
+		));
 
 		$this->assertWPErrorCode('tf-no-permission', tangible_fields()->store_value('test', 'store'));
 		$this->assertWPErrorCode('tf-no-permission', tangible_fields()->fetch_value('test'));
@@ -121,12 +123,14 @@ class Store_TestCase extends TF_UnitTestCase {
 	public function test_fields_store_permission_custom_permission() {
 		$allowed = false;
 
-		tangible_fields()->register_field('test', [
-			...tangible_fields()->_store_callbacks['memory'](),
-			'permission_callback' => function() use (&$allowed) {
-				return $allowed;
-			},
-		]);
+		tangible_fields()->register_field('test', array_merge(
+			tangible_fields()->_store_callbacks['memory'](),
+			[
+        'permission_callback' => function() use (&$allowed) {
+				  return $allowed;
+			  },
+      ]
+		));
 
 		$this->assertWPErrorCode('tf-no-permission', tangible_fields()->store_value('test', 'store'));
 		$this->assertWPErrorCode('tf-no-permission', tangible_fields()->fetch_value('test'));
@@ -143,19 +147,17 @@ class Store_TestCase extends TF_UnitTestCase {
 	}
 
 	public function test_fields_store_permission_undefined() {
-		tangible_fields()->register_field('test', [
-			...tangible_fields()->_store_callbacks['memory'](),
-		]);
+		tangible_fields()->register_field('test', tangible_fields()->_store_callbacks['memory']());
 
 		$this->assertWPErrorCode('tf-no-permission', tangible_fields()->store_value('test', 'store'));
 		$this->assertWPErrorCode('tf-no-permission', tangible_fields()->fetch_value('test'));
 	}
 
 	public function test_fields_store_permission_callback_empty() {
-		tangible_fields()->register_field('test', [
-			...tangible_fields()->_store_callbacks['memory'](),
-			...tangible_fields()->_permission_callbacks([]),
-		]);
+		tangible_fields()->register_field('test', array_merge(
+			tangible_fields()->_store_callbacks['memory'](),
+			tangible_fields()->_permission_callbacks([])
+		));
 
 		$this->assertWPErrorCode('tf-no-permission', tangible_fields()->store_value('test', 'store'));
 		$this->assertWPErrorCode('tf-no-permission', tangible_fields()->fetch_value('test'));
@@ -167,19 +169,19 @@ class Store_TestCase extends TF_UnitTestCase {
 			$errored[] = [$errno, $errstr, $args];
 		});
 
-		tangible_fields()->register_field('test', [
-			...tangible_fields()->_store_callbacks['memory'](),
-			...tangible_fields()->_permission_callbacks([
+		tangible_fields()->register_field('test', array_merge(
+			tangible_fields()->_store_callbacks['memory'](),
+			tangible_fields()->_permission_callbacks([
 				'store' => 'unknown',
-				'fetch' => 'unknown',
-			]),
-		]);
+				'fetch' => 'unknown'
+			])
+		));
 
 		restore_error_handler();
 
 		$this->assertNotNull($errored, 'unknown permission callback did not trigger a warning');
-		$this->assertCount(2, $errored);
-		$this->assertEquals($errored[0], $errored[1], 'the two warnings are not the same');
+		$this->assertCount(2, $errored);    
+    $this->assertEquals($errored[0], $errored[1], 'the two warnings are not the same');
 		[$errno, $errstr, $args] = $errored[0];
 
 		$this->assertEquals(E_USER_WARNING, $errno, 'unknown permission callback did not trigger an E_USER_WARNING');
@@ -189,13 +191,13 @@ class Store_TestCase extends TF_UnitTestCase {
 	}
 
 	public function test_fields_store_permission_callbacks_always_allow() {
-		tangible_fields()->register_field('test', [
-			...tangible_fields()->_store_callbacks['memory'](),
-			...tangible_fields()->_permission_callbacks([
-				'store' => 'always_allow',
-				'fetch' => 'always_allow',
-			]),
-		]);
+		tangible_fields()->register_field('test', array_merge(
+      tangible_fields()->_store_callbacks['memory'](),
+      tangible_fields()->_permission_callbacks([
+        'store' => 'always_allow',
+        'fetch' => 'always_allow',
+      ])
+    ));
 
 		$this->assertTrue(tangible_fields()->store_value('test', 'store'));
 		$this->assertEquals('store', tangible_fields()->fetch_value('test'));
@@ -209,13 +211,13 @@ class Store_TestCase extends TF_UnitTestCase {
 			return $password;
 		};
 
-		tangible_fields()->register_field('test', [
-			...tangible_fields()->_store_callbacks['memory'](),
-			...tangible_fields()->_permission_callbacks([
+		tangible_fields()->register_field('test', array_merge(
+			tangible_fields()->_store_callbacks['memory'](),
+			tangible_fields()->_permission_callbacks([
 				'store' => ['user_can', 'letmein', 'hunter2', $current_password],
 				'fetch' => 'always_allow',
-			]),
-		]);
+			])
+		));
 
 		add_filter('map_meta_cap', $map_meta_cap_filter = function($caps, $cap, $user_id, $args) {
 			list ($expected, $actual) = $args;
@@ -235,16 +237,18 @@ class Store_TestCase extends TF_UnitTestCase {
 	}
 
 	public function test_fields_store_validation_callbacks_required() {
-		tangible_fields()->register_field('test', [
-			...tangible_fields()->_store_callbacks['memory'](),
-			...tangible_fields()->_permission_callbacks([
+		tangible_fields()->register_field('test', array_merge(
+			tangible_fields()->_store_callbacks['memory'](),
+			tangible_fields()->_permission_callbacks([
 				'store' => 'always_allow',
 				'fetch' => 'always_allow',
 			]),
-			'validation_callbacks' => [
-				tangible_fields()->_validation_callback('required'),
-			],
-		]);
+			[
+        'validation_callbacks' => [
+				  tangible_fields()->_validation_callback('required'),
+        ]
+      ]
+		));
 
 		$this->assertWPErrorCode('tf-validation', tangible_fields()->store_value('test', ''));
 		$this->assertTrue(tangible_fields()->store_value('test', 'store'));
@@ -253,16 +257,18 @@ class Store_TestCase extends TF_UnitTestCase {
 	}
 
 	public function test_fields_store_validation_callbacks_required_custom_message() {
-		tangible_fields()->register_field('test', [
-			...tangible_fields()->_store_callbacks['memory'](),
-			...tangible_fields()->_permission_callbacks([
+		tangible_fields()->register_field('test', array_merge(
+			tangible_fields()->_store_callbacks['memory'](),
+			tangible_fields()->_permission_callbacks([
 				'store' => 'always_allow',
 				'fetch' => 'always_allow',
 			]),
-			'validation_callbacks' => [
-				tangible_fields()->_validation_callback('required', 'You MUST fill this in!'),
-			],
-		]);
+			[
+        'validation_callbacks' => [
+				  tangible_fields()->_validation_callback('required', 'You MUST fill this in!'),
+			  ],
+      ]
+		));
 
 		$this->assertWPErrorCode('tf-validation', $error = tangible_fields()->store_value('test', ''));
 		$this->assertEquals('You MUST fill this in!', $error->get_error_message());
@@ -314,16 +320,18 @@ class Store_TestCase extends TF_UnitTestCase {
 	}
 
 	public function test_fields_store_ajax_store() {
-		tangible_fields()->register_field('test', [
-			...tangible_fields()->_store_callbacks['memory'](),
-			...tangible_fields()->_permission_callbacks([
+		tangible_fields()->register_field('test', array_merge(
+			tangible_fields()->_store_callbacks['memory'](),
+			tangible_fields()->_permission_callbacks([
 				'store' => 'always_allow',
 				'fetch' => 'always_allow',
 			]),
-			'validation_callbacks' => [
-				tangible_fields()->_validation_callback('required', 'You MUST fill this in!'),
-			],
-		]);
+      [
+			  'validation_callbacks' => [
+				  tangible_fields()->_validation_callback('required', 'You MUST fill this in!'),
+			  ],
+      ]
+		));
 
 		$_GET['name'] = 'test';
 		$response = tangible_fields()->_ajax_store_callback();
@@ -346,13 +354,13 @@ class Store_TestCase extends TF_UnitTestCase {
 		unset($_REQUEST['value']);
 
 		tangible_fields()->registered_fields = [];
-		tangible_fields()->register_field('test', [
-			...tangible_fields()->_store_callbacks['memory'](),
-			...tangible_fields()->_permission_callbacks([
+		tangible_fields()->register_field('test', array_merge(
+			tangible_fields()->_store_callbacks['memory'](),
+			tangible_fields()->_permission_callbacks([
 				'store' => 'always_allow',
 				'fetch' => 'always_allow',
-			]),
-		]);
+			])
+		));
 
 		$response = tangible_fields()->_ajax_store_callback();
 		$this->assertNull(tangible_fields()->fetch_value('test'));
@@ -392,13 +400,13 @@ class Store_TestCase extends TF_UnitTestCase {
 	}
 
 	public function test_fields_store_ajax_fetch() {
-		tangible_fields()->register_field('test', [
-			...tangible_fields()->_store_callbacks['memory'](),
-			...tangible_fields()->_permission_callbacks([
+		tangible_fields()->register_field('test', array_merge(
+			tangible_fields()->_store_callbacks['memory'](),
+			tangible_fields()->_permission_callbacks([
 				'store' => 'always_allow',
 				'fetch' => 'always_allow',
-			]),
-		]);
+			])
+		));
 
 		$_GET['name'] = 'test';
 
