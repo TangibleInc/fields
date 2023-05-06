6c119e3309a77b061911bfeae1af656c3e580e20
Nicolas Jaussaud <nicolas@teamtangible.com>
2023-05-05 23:42:52 -0400

    Notice: Prefix classes

--- assets/src/components/base/notice/Notice.jsx
+++ assets/src/components/base/notice/Notice.jsx
@@ -1,8 +1,8 @@
 function Notice({ message, type, onDismiss }) {
   return (
-    <div className={`notice ${type} is-dismissible`}>
+    <div className={`tf-notice ${type} tf-is-dismissible`}>
       <p>{message}</p>
-      <button type="button" className="notice-dismiss" onClick={onDismiss}></button>
+      <button type="button" className="tf-notice-dismiss" onClick={onDismiss}></button>
     </div>
   )
 }
