df4f3845546a6387f9a5ac1fa726df07ad544514
Nicolas Jaussaud <nicolas@teamtangible.com>
2023-05-18 20:05:02 -0400

    Repeater block: Move bulk UI to a seperate componenet and state management to the dispatcher (prepare for future implementation in other layout); Add basic style; Fix checkbox and switch state update

--- assets/src/components/repeater/layout/block/Block.jsx
+++ assets/src/components/repeater/layout/block/Block.jsx
@@ -1,12 +1,13 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 import { 
   Button, 
   ModalTrigger,
   ExpandablePanel 
 } from '../../../base'
+
 import { Checkbox, Switch } from '../../../field'
-import Select from '../../../field/select'
+import BulkActions from '../../common/BulkActions'
 
 const Block = ({
   items,
@@ -21,53 +22,29 @@ const Block = ({
 
   const [activeItem, setActiveItem] = useState(0)
   const toggleItem = i => setActiveItem( i !== activeItem ? i : false )
-  const [selectedItems, setSelectedItems] = useState([])
 
-  const [valueSelect, setValueSelect] = useState('')
+  const bulkOptions = { 'deletion': 'Delete' }
 
-  let bulkOptions = []
-  bulkOptions['delete'] = 'delete'
-  
   if ( useSwitch ) {
-    bulkOptions['enabled'] = 'enabled'
-    bulkOptions['disabled'] = 'disabled'
-  }  
-
-  const isEnabled = ( item, i, value, controlName ) => (
-    dispatch({ 
-      type    : 'update',
-      item    : i,
-      control : controlName,
-      value   : value
-    })
-  )
-
-  const handleSelectAll = (value) => {
-    if( value !== '0' && value !== false ) {
-      setSelectedItems( [...items.map((item) => item)] )
-    } else {
-      setSelectedItems( [] )
-    }
-  };
-
-  const handleSelectItem = (value, item) => {
-    // let index = items.indexOf(item)
-    if ( value === '0' || value === false ) {
-      setSelectedItems(selectedItems.filter(id => id !== item))
-    } else {
-      if( ! selectedItems.includes(item) ) setSelectedItems([...selectedItems, item])
-    }
+    bulkOptions['enabled'] = 'Enabled'
+    bulkOptions['disabled'] = 'Disabled'
   }
 
-  const getHeaderLeft = ( item, i, activeCheckbox ) => {
+  const getHeaderLeft = (item, i) => {
     return (
       <>
         {
           useBulk
           ? <div onClick={ e => e.stopPropagation() }>
-              <Checkbox value={ activeCheckbox ?? '0' } isSelected={ activeCheckbox === '1' ? true : false } onChange={ value => {
-                handleSelectItem(value, item)
-              }} />
+              <Checkbox 
+                value={ item._bulkCheckbox }
+                onChange={ value => dispatch({ 
+                  type    : 'update',
+                  item    : i,
+                  control : '_bulkCheckbox',
+                  value   : value
+                }) } 
+              />
             </div>  
           : null
         }
@@ -75,9 +52,13 @@ const Block = ({
           useSwitch
           ? <div onClick={ e => e.stopPropagation() }>
               <Switch 
-                value={ item['repeater_switch'] ?? 'off' } 
-                isSelected={ item['repeater_switch'] === 'on' || item['repeater_switch'] === true ? true : false } 
-                onChange={ value => isEnabled( item, i, value, 'repeater_switch' ) }  
+                value={ item.enabled }
+                onChange={ value => dispatch({ 
+                  type    : 'update',
+                  item    : i,
+                  control : 'enabled',
+                  value   : value
+                }) }
               />
             </div>  
           : null 
@@ -106,7 +87,7 @@ const Block = ({
         <ModalTrigger 
           title="Confirmation"
           label="Remove"
-          onValidate={ () => dispatch({ type : 'remove', item : i })}
+          onValidate={ () => dispatch({ type : 'remove', item : i }) }
         >
           Are you sure you want to remove item { i + 1 }?
         </ModalTrigger> }
@@ -114,84 +95,23 @@ const Block = ({
   )
 
   return(
-    <div class='tf-repeater-block-items'>
-      {
-        useBulk ? 
-          <>
-            <Checkbox value={ '0' } onChange={ (value) => handleSelectAll(value) } />
-            <Select
-              placeholder="Bulk actions"
-              choices={ bulkOptions }
-              value={ valueSelect }
-              onChange={(value) => {
-                setValueSelect(value)
-              }}
-            />
-            {
-              valueSelect && valueSelect === 'delete' ?
-                <ModalTrigger 
-                  title="Confirmation"
-                  label="Apply"
-                  onValidate={ () => {
-                    selectedItems.sort((a,b) => {
-                      return items.indexOf(a) < items.indexOf(b) ? true : false
-                    })
-                    selectedItems.map((item) => {
-                      let i = items.indexOf(item)
-                      dispatch({ 
-                        type    : 'remove',
-                        item    : i,
-                      })
-                    })
-                    setValueSelect('')
-                    setSelectedItems([])
-                  } }
-                >
-                  Are you sure you want to remove selected items?
-                </ModalTrigger> : 
-                <Button 
-                  type="action"
-                  onPress={() => {
-                    switch (valueSelect) {
-                      case 'enabled':
-                        selectedItems.map((item) => {
-                          let i = items.indexOf(item)
-                          isEnabled( item, i, 'on', 'repeater_switch' )
-                        })
-                        setValueSelect('')
-                        setSelectedItems([])
-                        break;
-    
-                      case 'disabled':
-                        selectedItems.map((item) => {
-                          let i = items.indexOf(item)
-                          isEnabled( item, i, 'off', 'repeater_switch' )
-                        })
-                        setValueSelect('')
-                        setSelectedItems([])
-                        break;  
-                    
-                      default:
-                        break;
-                    }
-    
-                  }}
-                >Apply</Button>
-            }
-          </>
-        : null
-      }
+    <div className='tf-repeater-block-items'>
+      { useBulk && 
+        <BulkActions
+          actions={ bulkOptions }
+          dispatch={ dispatch }
+        /> }
       { items && items.slice(0, maxLength).map((item, i) => (
         <ExpandablePanel
           key={ item.key } 
           title={ title ?? 'Item ' + (i + 1) }
           footer={ actions(i, item) }
           isOpen={ activeItem === i }
-          class="tf-repeater-block-item"
+          className="tf-repeater-block-item"
           onChange={ visible => visible 
-            ? (activeItem !== i ? setActiveItem(i) : null) 
+            ? (activeItem !== i ? setActiveItem(i) : null)
             : (activeItem === i ? setActiveItem(false) : null) }
-          headerLeft={ getHeaderLeft( item, i, selectedItems.includes(item) ? '1' : '0' ) }
+          headerLeft={ getHeaderLeft(item, i) }
         > 
           { getRow(item).map(
             control => ( 
