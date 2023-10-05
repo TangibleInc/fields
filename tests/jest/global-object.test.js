d4fe122795e5c79c1e36cf01aebfe6d8cfde08ee
Nicolas Jaussaud <nicolas@teamtangible.com>
2023-10-04 21:58:38 -0400

    Tests: Start tests on the JS/react side using jest

---
+++ tests/jest/global-object.test.js
@@ -0,0 +1,65 @@
+import '../../assets/src/index.jsx'
+import { render } from '@testing-library/react'
+
+describe('window.tangibleFields', () => {
+  
+  it('exists', () => {
+    expect(window.tangibleFields).toBeDefined()
+  })
+
+  it('can render fields', () => {
+    
+    const fields = window.tangibleFields
+
+    expect(typeof fields.render).toBe('function')
+
+    const { container } = render(
+      fields.render({
+        label : 'Label',
+        type  : 'text',
+        name  : 'test-field' 
+      })
+    )
+    
+    const classes = container.firstChild.classList
+    expect(classes.contains('tf-context-default')).toEqual(true)
+  })
+
+  it('can trigger and subscribe to events', () => {
+    
+    const fields = window.tangibleFields
+
+    expect(typeof fields.event).toBe('function')
+    expect(typeof fields.trigger).toBe('function')
+    
+    let eventCalled = 0
+    fields.event('test-event', () => {
+      eventCalled++
+    })
+
+    fields.trigger('test-event')
+    expect(eventCalled).toBe(1)
+
+    fields.trigger('test-event')
+    expect(eventCalled).toBe(2)
+  })
+
+  it('can access the fields values', () => {
+    
+    const fields = window.tangibleFields
+
+    expect(typeof fields.store).toBe('object')
+    
+    render(
+      fields.render({
+        label : 'Label',
+        type  : 'text',
+        name  : 'test-field-store',
+        value : 'test-field-store-value'
+      })
+    )
+
+    expect(fields.store.getValue('test-field-store')).toBe('test-field-store-value')
+  })
+
+})
---
+++ tests/jest/setup/window.js
@@ -0,0 +1,19 @@
+/**
+ * Set up window.TangibleFields object as it would usually be created by wp_add_inline_script()
+ * 
+ * @see $fields->enqueue() in ../enqueue.php
+ */
+window.TangibleFields = {
+  api: {
+    nonce: '',  
+    endpoint: {
+      media: ''
+    }  
+  },
+  fields: [],
+  dynamics: {
+    values: [],
+    categories: []
+  },
+  mimetypes: {},
+}
