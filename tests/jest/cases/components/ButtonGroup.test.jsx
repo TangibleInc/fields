7ec9c133fcde3eba726adf9324a4ad36074f9b9e
Nicolas Jaussaud <nicolas@teamtangible.com>
2023-12-04 14:40:41 -0500

    Tests: Fix warings/format

--- tests/jest/cases/components/ButtonGroup.test.jsx
+++ tests/jest/cases/components/ButtonGroup.test.jsx
@@ -1,61 +1,69 @@
 import '../../../../assets/src/index.jsx'
-import { render, screen } from '@testing-library/react'
-import { renderHasElement, renderHasNotElement } from '../../utils/fields.js'
+import { render } from '@testing-library/react'
+import {
+  rendersWithMinimal,
+  rendersWithoutLabelThrowWarning,
+  rendersLabelAndDescription,
+  renderHasElement,
+  renderHasNotElement
+} from '../../utils/fields.js'
 
 const fields = window.tangibleFields
 
 describe('Button group component', () => {
 
-    it('renders', () => {
-      
-        const container = render( 
-          <>
-            { fields.render({
-              label       : 'Button group',
-              name        : 'button-group-name',
-              type        : 'button-group',
-              choices     : {
-                  choice1 : 'Choice 1', 
-                  choice2 : 'Choice 2'
-              }
-            }) }
-          </>
-        )
+  it('renders with minimal config', () => rendersWithMinimal({ type: 'button-group' }))
+  it('renders when no label but throws a warning', () => rendersWithoutLabelThrowWarning({ type: 'button-group' }))
+  it('renders label and description', () => rendersLabelAndDescription({ type: 'button-group' }))
+  
+  it('renders', () => {
+    
+      render( 
+        <>
+          { fields.render({
+            label   : 'Button group',
+            name    : 'button-group-name',
+            type    : 'button-group',
+            choices : {
+              choice1 : 'Choice 1', 
+              choice2 : 'Choice 2'
+            }
+          }) }
+        </>
+      )
 
-        // console.log('container : ', container)
+      const select = document.getElementsByClassName('tf-button-group')
+      expect(select.length).toBe(1)
 
-        const select = document.getElementsByClassName('tf-button-group')
-        expect(select.length).toBe(1)
+      const items = select[0].getElementsByClassName('tf-button-group-option')
+      expect(items.length).toBe(2)
 
-        const items = select[0].getElementsByClassName('tf-button-group-option')
-        expect(items.length).toBe(2)
+  })
 
-    })
+  it('supports readOnly', () => {
 
-    it('supports readOnly', () => {
+    const config = { 
+      label   : 'Button group',
+      type    : 'button-group',
+      choices : {
+        choice1 : 'Choice 1', 
+        choice2 : 'Choice 2'
+      }, 
+    }
 
-        const config = { 
-            label       : 'Button group',
-            type        : 'button-group',
-            choices     : {
-                choice1 : 'Choice 1', 
-                choice2 : 'Choice 2'
-            }, 
-        }
+    renderHasElement(config, container => container.querySelector('.tf-button-group-container'))
+    renderHasNotElement(config, container => container.querySelector('.tf-button-group-container[aria-disabled]'))
 
-        renderHasElement(config, container => container.querySelector('.tf-button-group-container'))
-        renderHasNotElement(config, container => container.querySelector('.tf-button-group-container[aria-disabled]'))
+    config.isDisabled = false
 
-        config.isDisabled = false
+    renderHasElement(config, container => container.querySelector('.tf-button-group-container'))
+    renderHasNotElement(config, container => container.querySelector('.tf-button-group-container[aria-disabled]'))
 
-        renderHasElement(config, container => container.querySelector('.tf-button-group-container'))
-        renderHasNotElement(config, container => container.querySelector('.tf-button-group-container[aria-disabled]'))
+    config.isDisabled = true
 
-        config.isDisabled = true
+    renderHasElement(config, container => container.querySelector('.tf-button-group-container'))
+    renderHasElement(config, container => container.querySelector('.tf-button-group-container[aria-disabled]'))
 
-        renderHasElement(config, container => container.querySelector('.tf-button-group-container'))
-        renderHasElement(config, container => container.querySelector('.tf-button-group-container[aria-disabled]'))
-
-    })
+  })
 
 })
--- tests/jest/cases/components/Select.test.jsx
+++ tests/jest/cases/components/Select.test.jsx
@@ -1,71 +1,96 @@
 import '../../../../assets/src/index.jsx'
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import { renderHasElement, renderHasNotElement } from '../../utils/fields.js'
+import { render, within } from '@testing-library/react'
+import { 
+  rendersWithoutLabelThrowWarning,
+  renderHasElement,
+  renderHasNotElement
+} from '../../utils/fields.js'
 
 const fields = window.tangibleFields
 
 describe('Select component', () => {
 
-    it('renders', () => {
-      
-        render( 
-            <>
-              { fields.render({
-                label       : 'Select',
-                name        : 'select-name',
-                type        : 'select',
-                choices     : [
-                  {
-                    choice1 : 'Choice 1' 
-                  },
-                  {
-                    choice2 : 'Choice 2'
-                  },
-                ]
-              }) }
-            </>
-        )
+  it('renders when no label but throws a warning', () => rendersWithoutLabelThrowWarning({ type: 'select' }))
+  
+  it('renders', () => {
+    
+    render( 
+      <>
+        { fields.render({
+          label   : 'Select',
+          name    : 'select-name',
+          type    : 'select',
+          choices : {
+            choice1 : 'Choice 1', 
+            choice2 : 'Choice 2'
+          }
+        }) }
+      </>
+    )
 
-        const select = document.getElementsByClassName('tf-select')
-        expect(select.length).toBe(1)
+    const select = document.getElementsByClassName('tf-select')
+    expect(select.length).toBe(1)
 
-        const items = select[0].getElementsByClassName('tf-button-select')
-        expect(items.length).toBe(1)
+    const items = select[0].getElementsByClassName('tf-button-select')
+    expect(items.length).toBe(1)
 
-        const currentChoice = items[0].textContent
-        expect(currentChoice).toContain('Select an option')
+    const currentChoice = items[0].textContent
+    expect(currentChoice).toContain('Select an option')
+  })
+ 
+  it('renders label and description', () => {
+    
+    const { container } = render( 
+      <>
+        { fields.render({
+          label       : 'Label for select',
+          description : 'Description for select',
+          name        : 'select-name',
+          type        : 'select',
+          choices     : {
+            choice1 : 'Choice 1', 
+            choice2 : 'Choice 2'
+          }
+        }) }
+      </>
+    )
 
-    })
+    const classes = container.firstChild.firstChild.classList
 
-    it('supports readOnly', () => {
+    expect(classes.contains(`tf-select`)).toEqual(true)
+  
+    const label = within(container).getAllByText(`Label for select`)
+    const description = within(container).getByText(`Description for select`)
+  
+    expect(label.length).toBe(2)
+    expect(description).toBeTruthy()
 
-      const config = { 
-        type    : 'select', 
-        label   : 'Label',
-        choices : [
-            {
-              choice1: 'Choice 1' 
-            },
-            {
-              choice2: 'Choice 2'
-            },
-        ] 
-      }
+  })
 
-      renderHasElement(config, container => container.querySelector('.tf-button-select'))
-      renderHasNotElement(config, container => container.querySelector('.tf-button-select[disabled]'))
+  it('supports readOnly', () => {
 
-      config.isDisabled = false
+    const config = { 
+      type    : 'select', 
+      label   : 'Label',
+      choices : {
+        choice1 : 'Choice 1', 
+        choice2 : 'Choice 2'
+      },
+    }
 
-      renderHasElement(config, container => container.querySelector('.tf-button-select'))
-      renderHasNotElement(config, container => container.querySelector('.tf-button-select[disabled]'))
+    renderHasElement(config, container => container.querySelector('.tf-button-select'))
+    renderHasNotElement(config, container => container.querySelector('.tf-button-select[disabled]'))
 
-      config.isDisabled = true
+    config.isDisabled = false
 
-      renderHasElement(config, container => container.querySelector('.tf-button-select'))
-      renderHasElement(config, container => container.querySelector('.tf-button-select[disabled]'))
+    renderHasElement(config, container => container.querySelector('.tf-button-select'))
+    renderHasNotElement(config, container => container.querySelector('.tf-button-select[disabled]'))
 
-    })
+    config.isDisabled = true
+
+    renderHasElement(config, container => container.querySelector('.tf-button-select'))
+    renderHasElement(config, container => container.querySelector('.tf-button-select[disabled]'))
+
+  })
 
 })
