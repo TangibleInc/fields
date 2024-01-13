62036a7c26ae143a33ada84bc701f2f7454cd5ad
Nicolas Jaussaud <nicolas@teamtangible.com>
2024-01-12 23:04:06 -0500

    Update version and changelog.md

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ new class extends stdClass {
   public $name = 'tangible_fields';
 
   // Remember to update the version - Expected format: YYYYMMDD
-  public $version = '20240112';
+  public $version = '20240113';
 
   function __construct() {
 
