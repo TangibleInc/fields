2907b783651be1d09a25a9269e841d9ce0cea5e5
Erica <erica@teamtangible.com>
2024-03-15 10:49:01 +0800

    Fixing the format

--- assets/src/contexts/elementor/index.js
+++ assets/src/contexts/elementor/index.js
@@ -8,39 +8,39 @@
  */
 
 const initElementor = () => {
-  const darkStyleSheet = document.getElementById('e-theme-ui-dark-css');
-  const lightStyleSheet = document.getElementById('e-theme-ui-light-css');
+  const darkStyleSheet = document.getElementById('e-theme-ui-dark-css')
+  const lightStyleSheet = document.getElementById('e-theme-ui-light-css')
 
   const setMode = theme => {
     // Remove all existing theme classes
-    document.body.classList.remove('tf-elementor-theme-light', 'tf-elementor-theme-dark');
+    document.body.classList.remove('tf-elementor-theme-light', 'tf-elementor-theme-dark')
   
     // Add the appropriate theme class
-    document.body.classList.add(`tf-elementor-theme-${theme}`);
-  };
+    document.body.classList.add(`tf-elementor-theme-${theme}`)
+  }
 
   const checkAndSetMode = () => {
-    setMode(darkStyleSheet && darkStyleSheet.media === 'all' ? 'dark' : 'light');
-  };
+    setMode(darkStyleSheet && darkStyleSheet.media === 'all' ? 'dark' : 'light')
+  }
 
   const observer = new MutationObserver(mutations => {
     for (const mutation of mutations) {
-      if (mutation.type !== 'attributes') continue;
+      if (mutation.type !== 'attributes') continue
 
-      const isDarkMode = darkStyleSheet && darkStyleSheet.media === 'all';
-      const isLightMode = lightStyleSheet && lightStyleSheet.media === 'all';
+      const isDarkMode = darkStyleSheet && darkStyleSheet.media === 'all'
+      const isLightMode = lightStyleSheet && lightStyleSheet.media === 'all'
 
-      if (isDarkMode) setMode('dark');
-      if (isLightMode) setMode('light');
+      if (isDarkMode) setMode('dark')
+      if (isLightMode) setMode('light')
     }
-  });
+  })
 
   // Observe changes to the media attribute of the stylesheets
-  observer.observe(darkStyleSheet, { attributes: true, attributeFilter: ['media'] });
-  observer.observe(lightStyleSheet, { attributes: true, attributeFilter: ['media'] });
+  observer.observe(darkStyleSheet, { attributes: true, attributeFilter: ['media'] })
+  observer.observe(lightStyleSheet, { attributes: true, attributeFilter: ['media'] })
 
   // Set initial mode
-  checkAndSetMode();
-};
+  checkAndSetMode()
+}
 
-export { initElementor };
+export { initElementor }
