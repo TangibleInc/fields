77ca24d0d6636e49ff50600de7df4ff333d39045
Julien Kappler <julien@teamtangible.com>
2024-03-18 14:51:38 +0100

    Field group : Fix issue with repeater

--- tests/jest/cases/components/controls/FieldGroup.test.jsx
+++ tests/jest/cases/components/controls/FieldGroup.test.jsx
@@ -1,4 +1,5 @@
 import '../../../../../assets/src/index.jsx'
+import userEvent from '@testing-library/user-event'
 import { render, within } from '@testing-library/react'
 
 const fields = window.tangibleFields
@@ -36,6 +37,48 @@ describe('Field group component', () => {
     
     expect(container.querySelector('.tf-text'))
     expect(within(container).getByText('Subfield 2')).toBeTruthy()
+
+  })
+
+  it('renders with repeater inside field', async () => {
+
+    const user = userEvent.setup()
+    const { container } = render( 
+      <>
+        { fields.render({
+          name   : 'field-group-name',
+          type   : 'field-group',
+          value  : '{"advanced":[{"advanced_text":"test"},{"key":"613b35d6811aa8","advanced_text":"test2"}]}',
+          fields : [
+            {
+              type    : 'repeater',
+              layout  : 'advanced',
+              name    : 'advanced',
+              fields  : [
+                {
+                  type  : 'text',
+                  name  : 'advanced_text'
+                }
+              ] 
+            }
+          ]
+        }) }
+      </>
+    ) 
+    
+    const fieldGroup = container.querySelectorAll('.tf-field-group')
+    expect(fieldGroup.length).toBe(1)
+
+    const openButton = container.querySelectorAll('.tf-button-repeater-overview-open')
+    expect(openButton)
+
+    await user.click(openButton[0])
+    expect(container.querySelector('.tf-text')).toBeTruthy()
+    expect(container.querySelectorAll('.tf-text').length).toBe(1)
+
+    await user.click(openButton[1])
+    expect(container.querySelector('.tf-text')).toBeTruthy()
+    expect(container.querySelectorAll('.tf-text').length).toBe(1)
 
   })
 })
