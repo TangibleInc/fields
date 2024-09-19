99c0d1182ad58280379018b15644faf7c8c5699c
Adrian Buenavista <adrian@teamtangible.com>
2024-09-19 15:19:17 +0800

    add dark and light theme color

--- assets/src/components/base/tooltip/Tooltip.jsx
+++ assets/src/components/base/tooltip/Tooltip.jsx
@@ -4,6 +4,7 @@ const Tooltip = props => {
 
   const content = props.content ?? children
   const placement = props.placement ?? 'top'
+  const theme = props.theme && props.theme === 'dark' ? 'dark' : 'light'
   const [ isOpen, setIsOpen ] = React.useState(true)
 
   return (
@@ -11,9 +12,9 @@ const Tooltip = props => {
     {
       isOpen &&
       ( <div className={`tf-tooltip tf-tooltip-${placement}`}>
-        <div className={`tf-tooltip-content tf-tooltip-content-${placement}`}>
+        <div className={`tf-tooltip-content tf-tooltip-content-${placement} ${theme}`}>
           { content }
-        </div>
+        </div> 
       </div> )
     }
     </>
