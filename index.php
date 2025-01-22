0f7e734210d22e7d7333c40ea01d6753bb73204d
titus-toia <titus-toia@users.noreply.github.com>
2025-01-22 17:53:56 +0200

    2025.01.22
    Force recompile

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ endif;
 
   public $name = 'tangible_fields';
   // Remember to update the version - Expected format: YYYYMMDD
-  public $version = '20250107';
+  public $version = '20250122';
 
   // Dynamic methods
   function __call( $method = '', $args = [] ) {
