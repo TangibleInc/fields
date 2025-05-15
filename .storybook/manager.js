aea60bea9289ec2dc07910d37735e30740c0a4e0
Julia Canzani <julia@teamtangible.com>
2025-05-14 20:01:50 -0400

    Add basic custom theme

---
+++ .storybook/manager.js
@@ -0,0 +1,7 @@
+import { addons } from '@storybook/manager-api';
+import { themes } from '@storybook/theming';
+import tangibleTheme from './tangibleTheme';
+ 
+addons.setConfig({
+  theme: tangibleTheme,
+});
---
+++ .storybook/tangibleTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Tangible Fields',
+  brandUrl: 'https://develop.tangible.one',
+  brandImage: 'https://teamtangible.com/wp-content/uploads/2022/11/Logo.svg',
+  brandTarget: '_self',
+});
