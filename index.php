84d3c26454d9784b51635324b007b6053bceb57b
eliot-akira <me@eliotakira.com>
2025-08-21 08:34:49 +0200

    Update version

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ endif;
 
   public $name = 'tangible_fields';
   // Remember to update the version - Expected format: YYYYMMDD
-  public $version = '20250719';
+  public $version = '20250821';
 
   // Dynamic methods
   function __call( $method = '', $args = [] ) {
--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Tangible Fields
  * Description: React-based custom fields library
- * Version: 2025.7.19
+ * Version: 2025.8.21
  */
 use tangible\updater;
 
