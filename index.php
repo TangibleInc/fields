416ec950bf2370852a3f9d12dd9d8df2b68c1594
Nicolas Jaussaud <nicolas@teamtangible.com>
2025-08-25 23:28:02 +0200

    20250825

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ endif;
 
   public $name = 'tangible_fields';
   // Remember to update the version - Expected format: YYYYMMDD
-  public $version = '20250821';
+  public $version = '20250825';
 
   // Dynamic methods
   function __call( $method = '', $args = [] ) {
