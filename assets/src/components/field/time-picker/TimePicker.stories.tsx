1acfa2c6227ba012be81c8ff82958a0180c67a26
Nicolas Jaussaud <nicolas@teamtangible.com>
2026-03-30 18:05:17 +0200

    Storybook: Add time-picker component - closes #39

---
+++ assets/src/components/field/time-picker/TimePicker.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { Field } from '../../../index'
+
+const meta = {
+  title: 'Fields (Legacy)/TimePicker',
+  component: Field,
+  decorators: [
+    Story => (
+      <div style={{ minWidth: '500px' }}>
+        <Story />
+      </div>
+    )
+  ],
+  parameters: {
+    layout: 'padded'
+  },
+  args: {
+    type: 'time-picker',
+    label: 'Time field',
+    description: 'Description',
+    hourCycle: 12,
+  }
+} satisfies Meta<typeof Field>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
+
+export const WithLimits: Story = {
+  args: {
+    min: '09:00',
+    max: '17:00',
+    description: 'You can only select a time between 9 AM to 5:00 PM',
+  }
+}
