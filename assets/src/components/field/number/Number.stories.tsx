997ed6faef247b99d233d88d79a17b559ba212d8
Nicolas Jaussaud <nicolas@teamtangible.com>
2026-03-30 16:50:41 +0200

    Storybook: Add number component - closes #31

---
+++ assets/src/components/field/number/Number.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { Field } from '../../../index'
+
+const meta = {
+  title: 'Fields (Legacy)/Number',
+  component: Field,
+  decorators: [
+    Story => (
+      <div style={{ minWidth: '500px' }}>
+        <Story />
+      </div>
+    )
+  ],
+  parameters: {
+    layout: 'padded'
+  },
+  args: {
+    type: 'number',
+    label: 'Number field',
+    description: 'Example description',
+  }
+} satisfies Meta<typeof Field>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
+
+export const WithMinMax: Story = {
+  args: {
+    minValue: 10,
+    maxValue: 20,
+  }
+}
